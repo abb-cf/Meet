006e9db3ddbd7a1da74760129549f4bb0886c6ca
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-01-26 20:21:30 -0500

    using template literals for gainging access to API rather  than string  concatenation

--- src/api.js
+++ src/api.js
@@ -44,7 +44,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url = 'https://qbbomao7r4.execute-api.us-east-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const url = `https://qbbomao7r4.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
@@ -79,7 +79,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
     const { access_token } = await fetch(
-        'https://qbbomao7r4.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+        `https://qbbomao7r4.execute-api.us-east-1.amazonaws.com/dev/api/token/${encodeCode}`
     )
         .then((res) => {
             return res.json();
